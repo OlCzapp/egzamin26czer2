58081d7c9fffbffdbcdc149361eebbaf22bbdb18
Oliwia <oliwia.czap13@gmail.com>
2026-02-04 12:37:05 +0100

    program cs, działające szyfrowanie

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,10 +23,35 @@ namespace egzamin26czer2
 
         private void zaszyfruj_Click(object sender, RoutedEventArgs e)
         {
+            /*
             int wartosc_klucza_ = int.Parse(wartosc_klucza.Text);
             string text_do_szyfru_ = text_do_szyfru.Text;
+            string zaszyfrowany_text_ = "mial";
 
-            MessageBox.Show(text_do_szyfru_+", "+ wartosc_klucza_);
+            MessageBox.Show("text: "+text_do_szyfru_+", klucz: "+ wartosc_klucza_+", szyfr: "+zaszyfrowany_text_);
+            */
+
+            int wartosc_klucza_cs = int.Parse(wartosc_klucza.Text);
+            string text_do_szyfru_cs = text_do_szyfru.Text;
+
+            string wynik = "";
+
+            foreach (char c in text_do_szyfru_cs)
+            {
+                if (char.IsLetter(c))
+                {
+                    char start = char.IsUpper(c) ? 'A' : 'a';
+                    char zaszyfrowany = (char)((c - start + wartosc_klucza_cs) % 26 + start);
+                    wynik += zaszyfrowany;
+                }
+                else
+                {
+                    wynik += c; // spacje, znaki specjalne
+                }
+            }
+
+            zaszyfrowany_text.Text = wynik;
+
         }
 
         private void zapisz_w_txt_Click(object sender, RoutedEventArgs e)
